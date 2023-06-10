daca0303545a32cb13cca4cdced64b57a156df8a
aya-se <kakkeno1@gmail.com>
2023-06-10 23:49:12 +0900

    Add frontend app

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ClickCounter from "./components/ClickCounter.vue";
+import ItemList from "./components/ItemList.vue";
+</script>
+
+<template>
+  <main>
+    <ClickCounter />
+    <ItemList />
+  </main>
+</template>
+
+<style lang="scss"></style>
