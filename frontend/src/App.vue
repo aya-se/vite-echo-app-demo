443c271a36b2e1312081e77c5fad3c396369660e
aya-se <kakkeno1@gmail.com>
2023-06-11 00:36:39 +0900

    Add backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import ClickCounter from "./components/ClickCounter.vue";
 import ItemList from "./components/ItemList.vue";
+import FoodList from "./components/FoodList.vue";
 </script>
 
 <template>
   <main>
     <ClickCounter />
     <ItemList />
+    <FoodList />
   </main>
 </template>
 
