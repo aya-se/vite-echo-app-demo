4ff9cac7514206f2161be4c590c8d310431e7dcb
aya-se <kakkeno1@gmail.com>
2023-06-17 00:03:01 +0900

    store設計の試行

--- src/pages/Edit.vue
+++ src/pages/Edit.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { store } from "../store.js";
+import { store, score } from "../store.js";
 import { ref } from "vue";
 
 const input = ref("");
 </script>
 <template>
   <div>Edit</div>
+  <div>{{ score.ika }}</div>
+  <button @click="score.ika++">いか</button>
+  <div>{{ score.shika }}</div>
+  <button @click="score.shika++">しか</button>
+  <div>{{ score.meka }}</div>
+  <button @click="score.meka++">めか</button>
+  <div>{{ score.total }}</div>
   <TransitionGroup name="list">
     <div v-for="item in store.globalList" :key="item">
       <p>{{ item }}</p>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,56 @@
-import { reactive } from "vue";
+import { computed, reactive, ref } from "vue";
 
 export const store = reactive({
   count: 0,
   globalList: ["りんご", "みかん", "バナナ"],
+});
+
+// ゲームの状態を管理する
+export const status = ref("title");
+
+// タイマーを管理する
+export const timer = reactive({
+  countDownTimer: 3,
+  gameTimer: 30,
+});
+
+// 得点を管理する
+export const score = reactive({
+  ika: 0,
+  shika: 0,
+  meka: 0,
+  total: computed(() => {
+    return score.ika + score.shika + score.meka;
+  }),
+  highest: 0,
+});
+
+// メッセージリストを管理する
+export const messages = reactive({
+  allList: [],
+  correctList: computed(() => {
+    return messages.allList.filter((message) => {
+      return message.ika || message.shika || message.meka;
+    });
+  }),
+  incorrectList: computed(() => {
+    return messages.allList.filter((message) => {
+      return !message.ika && !message.shika && !message.meka;
+    });
+  }),
+
+  // ゲーム画面用
+  leftList: [],
+  middleList: [],
+  rightList: [],
+
+  // リザルト画面用
+  resultIkaList: [],
+  resultShikaList: [],
+  resultMekaList: [],
+});
+
+// ランキングを管理する
+export const ranking = reactive({
+  list: [],
 });
